9191bf775ec4581ba1110e8e5c78daa3bd4ac2aa
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-03-30 14:49:48 +0530

    Allow support for multiple github admins

--- src/server/controller/user.js
+++ src/server/controller/user.js
@@ -39,7 +39,8 @@ router.get('/auth/github', checkReturnTo);
 // router.get('/auth/github/callback', passport.authenticate('github', { successReturnToOrRedirect: '/', failureRedirect: '/' }));
 router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
     function (req, res) {
-        if(req.user && req.session.requiredScope != 'public' && req.user.login == config.server.github.admin_user && ( !req.user.scope || req.user.scope.indexOf('write:repo_hook') < 0 )) {
+        if(req.user && req.session.requiredScope != 'public' && ( !req.user.scope || req.user.scope.indexOf('write:repo_hook') < 0 ) &&
+          (config.server.github.admin_users.length > 0 && config.server.github.admin_users.indexOf(req.user.login) >= 0)) {
             return res.redirect('/auth/github?admin=true');
         }
         res.redirect(req.session.returnTo || '/');
